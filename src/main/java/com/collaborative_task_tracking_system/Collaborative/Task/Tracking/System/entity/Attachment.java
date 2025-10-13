d0769b3d80866caa0d76078631ed4e869687f2fe
shreyash0185 <shreyashmuktalkar98@gmail.com>
2025-10-13 17:02:07 +0530

    added change into Entity

--- src/main/java/com/collaborative_task_tracking_system/Collaborative/Task/Tracking/System/entity/Attachment.java
+++ src/main/java/com/collaborative_task_tracking_system/Collaborative/Task/Tracking/System/entity/Attachment.java
@@ -1,13 +1,18 @@
 package com.collaborative_task_tracking_system.Collaborative.Task.Tracking.System.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Document(collection = "attachments")
 public class Attachment {
 
+    @Id
+    private String id;
     private String fileName;
     private String fileType;
     private String fileUrlOrPath;
--- src/main/java/com/collaborative_task_tracking_system/Collaborative/Task/Tracking/System/entity/Comment.java
+++ src/main/java/com/collaborative_task_tracking_system/Collaborative/Task/Tracking/System/entity/Comment.java
@@ -1,12 +1,19 @@
 package com.collaborative_task_tracking_system.Collaborative.Task.Tracking.System.entity;
 
 import lombok.*;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "comments")
 public class Comment {
 
+    @Id
+    private String id;  
     private String userId; // user who wrote the comment
     private String content;
     private LocalDateTime createdAt;
